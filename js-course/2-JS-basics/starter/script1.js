7ba731a84b2c40d55e815a4587b44a85e538e087
Enda Kelly <ekel@Endas-MacBook-Pro.local>
2020-01-12 22:19:12 +0000

    class 1 complete

---
+++ js-course/2-JS-basics/starter/script1.js
@@ -0,0 +1,25 @@
+/*
+var firstName = "John";
+console.log(firstName);
+
+var lastName = "Smith";
+var age = 28;
+
+var fullAge = true;
+console.log(fullAge);
+
+*/
+// type coercion
+var firstName = "Enda";
+var age = "33";
+
+console.log(firstName + " " + age);
+
+var job, isMarried;
+job = "engineer";
+isMarried = "false"
+
+console.log(firstName + ' is a ' +  age + ' year old ' + job + ' . ' + 'Is this person married? ' + ' : ' + isMarried);
+
+// type mutation
+var age = "thirty three"
